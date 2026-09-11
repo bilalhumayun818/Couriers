d87d30d4cc7d83e899b2ce166f2d62853cfda721
SafiullahZafar <safullahzafar@gmail.com>
2026-09-11 05:32:30 -0700

    make change in app/services/demourlgenerator

--- app/Services/DemoUrlGenerator.php
+++ app/Services/DemoUrlGenerator.php
@@ -8,11 +8,31 @@ class DemoUrlGenerator extends BaseUrlGenerator
 {
     public function route($name, $parameters = [], $absolute = true)
     {
-        if (is_string($name) && !str_starts_with($name, 'demo.') && request() && request()->is('demo*')) {
+        if (
+            is_string($name) &&
+            !str_starts_with($name, 'demo.') &&
+            request() &&
+            request()->is('demo*')
+        ) {
             if ($this->routes->getByName('demo.' . $name)) {
                 $name = 'demo.' . $name;
             }
         }
-        return parent::route($name, $parameters, $absolute);
+
+        $url = parent::route($name, $parameters, $absolute);
+
+        if (request() && request()->is('courier*')) {
+            if ($absolute) {
+                $url = str_replace(
+                    'http://187.127.204.45/',
+                    'http://187.127.204.45/courier/',
+                    $url
+                );
+            } else {
+                $url = '/courier' . $url;
+            }
+        }
+
+        return $url;
     }
 }
